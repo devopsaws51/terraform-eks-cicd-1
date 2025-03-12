2e7b5a45ebe3dbc3578cc8acd8a2cc8295ab2cf7
devopsaws51 <devopsaws512@gmail.com>
2025-03-12 15:31:11 +0530

    Update backend.tf

--- tf-aws-eks/backend.tf
+++ tf-aws-eks/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-eks-cicd-7001"
+    bucket = "terraform-eks-cicd-70020"
     key    = "eks/terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-south-1"
   }
 }
